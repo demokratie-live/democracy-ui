fd70eee7c7b095c527af54d36a203ca919de7e52
ManAnRuck <github@manuelruck.de>
2021-04-09 17:23:56 +0200

    cleanup
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,10 +1,8 @@
-/** TEST */
-export * from "./Example";
-
 /** THEME */
 export * from "./theme";
 
 /** Atoms */
+export * from "./Atoms/Button";
+export * from "./Atoms/Button/VoteButton";
 export * from "./Atoms/ListItem/VotesIndex";
 export * from "./Atoms/PieChart";
-export * from "./Atoms/Button/VoteButton";
