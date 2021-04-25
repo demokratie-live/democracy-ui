abf287fb6da816165c209e0fb8cd5215b0d8f99c
ManAnRuck <github@manuelruck.de>
2021-04-25 10:41:39 +0200

    improve CountryMap
    show constituencies shy of there are no vote results
    
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- packages/ui/src/Molecules/CountryMap/index.tsx
+++ packages/ui/src/Molecules/CountryMap/index.tsx
@@ -13,7 +13,21 @@ const Container = styled.View`
 `;
 
 export interface CountryMapProps {
-  data: any;
+  data: {
+    procedure: {
+      procedureId: string;
+      voted: boolean;
+      communityVotes: {
+        constituencies: Array<{
+          yes: number;
+          no: number;
+          abstination: number;
+          total: number;
+          constituency: string;
+        }>;
+      } | null;
+    };
+  };
 }
 
 export const CountryMap: React.FC<CountryMapProps> = ({ data }) => {
@@ -26,43 +40,61 @@ export const CountryMap: React.FC<CountryMapProps> = ({ data }) => {
   const allConstituencies = data.procedure.communityVotes;
 
   const maxVotersConstituency = max(
-    allConstituencies.constituencies.map(({ total }: any) => total),
+    allConstituencies.constituencies.map(({ total }) => total),
   );
 
   return (
     <Container>
       <CountryMapSvg width={width / 1.5}>
-        {allConstituencies.constituencies.map(
-          ({ constituency, yes, abstination, no, total }: any) => {
-            const yesNoValue = (yes - no) / total;
-            const abstinationValue = abstination / total / 2;
+        {Object.keys(constituencySvgs).map((constituencyI) => {
+          const constituencyData = allConstituencies.constituencies.find(
+            ({ constituency }) => constituency === constituencyI,
+          );
+          const {
+            constituency,
+            yes,
+            abstination,
+            no,
+            total,
+          } = constituencyData || {
+            constituency: constituencyI,
+            yes: 0,
+            abstination: 0,
+            no: 0,
+            total: 0,
+          };
 
-            const colorValue =
-              yesNoValue === 0
-                ? 0
-                : yesNoValue > 0
-                ? yesNoValue - abstinationValue
-                : yesNoValue + abstinationValue;
-            const colorRange = scaleLinear()
-              .domain([-1, 0, 1])
-              .range([
-                (theme.colors.vote.community.no as unknown) as number,
-                (theme.colors.vote.community.abstination as unknown) as number,
-                (theme.colors.vote.community.yes as unknown) as number,
-              ]);
-            const opacityRange = scaleLinear().domain([0, 1]).range([0.3, 1]);
-            return (
-              <G
-                key={`constituency-${constituency}`}
-                opacity={opacityRange(total / (maxVotersConstituency as any))}
-              >
-                {(constituencySvgs as any)[constituency](
-                  colorRange(colorValue),
-                )}
-              </G>
-            );
-          },
-        )}
+          const yesNoValue = (yes - no) / total;
+          const abstinationValue = abstination / total / 2;
+
+          const colorValue =
+            yesNoValue === 0
+              ? 0
+              : yesNoValue > 0
+              ? yesNoValue - abstinationValue
+              : yesNoValue + abstinationValue;
+          const colorRange = scaleLinear()
+            .domain([-1, 0, 1])
+            .range([
+              (theme.colors.vote.community.no as unknown) as number,
+              (theme.colors.vote.community.abstination as unknown) as number,
+              (theme.colors.vote.community.yes as unknown) as number,
+            ]);
+          const opacityRange = scaleLinear().domain([0, 1]).range([0.3, 1]);
+          return (
+            <G
+              key={`constituency-${constituency}`}
+              opacity={
+                total ? opacityRange(total / (maxVotersConstituency || 0)) : 0.1
+              }
+            >
+              {(constituencySvgs as any)[constituency]({
+                fill: colorRange(colorValue),
+                stroke: theme.colors.background.secondary,
+              })}
+            </G>
+          );
+        })}
       </CountryMapSvg>
     </Container>
   );
